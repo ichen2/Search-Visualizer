1b2d0afd04043d69c1f17654dc381e7fbfbc8ce8
Ian <ichen2@lsu.edu>
2021-02-13 15:23:45 -0600

    Implemented a bfs that searches the whole grid, and doesn't stop at the target. Animated bfs

--- src/SearchVisualizer/SearchVisualizer.js
+++ src/SearchVisualizer/SearchVisualizer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './SearchVisualizer.css';
 import { Node } from './Node/Node.js';
 import { bfs } from '../algorithms/searchAlgorithms.js'
@@ -6,15 +6,20 @@ import { bfs } from '../algorithms/searchAlgorithms.js'
 const NUM_ROWS = 20;
 const NUM_COLS = 40;
 
-
-export class SearchVisualizer extends React.Component {
+export class SearchVisualizer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            nodes: []
+            nodes: [], 
+            startNode: {x: 10, y: 10},
+            endNode: {x: 30, y: 10}
         };
+        this.visualizeBFS = this.visualizeBFS.bind(this);
+        this.animateBFS = this.animateBFS.bind(this);
     }
     componentDidMount() {
+        let {x: startX, y: startY} = this.state.startNode;
+        let {x: endX, y: endY} = this.state.endNode;
         const nodes = [];
         for(let row = 0; row < NUM_ROWS; row++) {
             let nodeRow = [];
@@ -22,8 +27,8 @@ export class SearchVisualizer extends React.Component {
                 const currentNode = {
                     col,
                     row,
-                    isStart: row === 10 && col == 10,
-                    isEnd: row === 10 && col == 30,
+                    isStart: row === startY && col == startX,
+                    isEnd: row === endY && col == endX,
                     isVisited: false
                 } 
                 nodeRow.push(currentNode);
@@ -32,11 +37,31 @@ export class SearchVisualizer extends React.Component {
         }
         this.setState({nodes});
     }
+    animateBFS(orderedVisitedNodes) {
+        console.log(orderedVisitedNodes);
+        for(let i = 0; i < orderedVisitedNodes.length; i++) {
+            setTimeout(() => {
+                const newGrid = this.state.nodes.slice();
+                orderedVisitedNodes[i].forEach((node) => {
+                    const newNode = {...node, isVisited: true};
+                    newGrid[node.row][node.col] = newNode;
+                });
+                this.setState({nodes: newGrid});
+            }, 100 * i);
+        }
+    }
+    visualizeBFS() {
+        let {x: startX, y: startY} = this.state.startNode;
+        let {x: endX, y: endY} = this.state.endNode;
+        const {nodes: grid} = this.state;
+        let orderedVisitedNodes = bfs(grid, grid[startY][startX], grid[endY][endX]);
+        this.animateBFS(orderedVisitedNodes);
+    }
     render() {
         let {nodes} = this.state;
         return (
             <div id="grid">
-                <button onClick={bfs()}>Click Me</button>
+                <button onClick={this.visualizeBFS}>Click Me</button>
                 {nodes.map((row, rowIndex) => {
                     return <div className="row" key={rowIndex}>
                         {row.map((node, nodeIndex) => 
--- src/algorithms/searchAlgorithms.js
+++ src/algorithms/searchAlgorithms.js
@@ -1,3 +1,55 @@
-export function bfs() {
-    console.log("bfs");
+export function bfs(nodes, start, end) {
+    let grid = copyGrid(nodes);
+    let visitedNodes = [[grid[start.row][start.col]]];
+    let index = 0;
+    while(visitedNodes[index].length > 0 && index < 50) {
+        let currentNodes = visitedNodes[index];
+        let neighborNodes = [];
+        currentNodes.forEach((node) => {
+            if(!node.isVisited) {
+                node.isVisited = true;
+                if(node.row === end.row && node.col === end.col) {
+                    visitedNodes.push(neighborNodes);
+                    return visitedNodes;
+                }
+                neighborNodes = neighborNodes.concat(getNeighbors(grid, node));
+            }
+        })
+        visitedNodes.push(neighborNodes); 
+        index++;
+    }
+    return visitedNodes;
+}
+
+function copyGrid(grid) {
+    let copy = [];
+    for(const row of grid) {
+        let copiedRow = [];
+        for(const node of row) {
+            const currentNode = {
+                ...node
+            }; 
+            copiedRow.push(currentNode);
+        }
+        copy.push(copiedRow);
+    }
+    return copy;
+}
+
+function getNeighbors(grid, node) {
+    let neighbors = []
+    let {row, col} = node;
+    if(row < grid.length -1) {
+        neighbors.push(grid[node.row+1][node.col]);
+    }
+    if(row > 0) {
+        neighbors.push(grid[node.row-1][node.col]);
+    }
+    if(col < grid[0].length - 1) {
+        neighbors.push(grid[node.row][node.col+1]);
+    }
+    if(col > 0) {
+        neighbors.push(grid[node.row][node.col-1]);
+    }
+    return neighbors.filter((neighbor) => !neighbor.isVisited);
 }
